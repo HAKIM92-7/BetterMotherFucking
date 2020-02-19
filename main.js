3e794b3b8516a5d89ad58bafb640764a78c2f92d
HAKIM <hakim@hakim-HP-EliteBook-8440p.com>
2020-02-19 08:31:28 +0100

    ajouter la fonction changement police

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const button = document.getElementById('button1');
 
 button.addEventListener('click', event => {
-  button.innerHTML = `Nombre de clics : ${event.detail}`;
+    button.innerHTML = `Nombre de clics : ${event.detail}`;   //nombre de click  
 });
 
 
@@ -16,22 +16,22 @@ let myImage = document.querySelector('img');
 
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/firefox.png') {
-      myImage.setAttribute('src', 'images/firefox2.png');
+    if (mySrc === 'images/beauty.png') {
+      myImage.setAttribute('src', 'images/beauty2.png');    //chagement d'image
     } else {
-      myImage.setAttribute('src', 'images/firefox.png');
+      myImage.setAttribute('src', 'images/beauty.png');
     }
 });
 
 
 
-let myButton = document.getElementById('button1');    //referencer sur le bouton crée
-let myHeading = document.querySelector('h1');      //referencer sur le titre de la page
+let myButton = document.getElementById('button1');    //reference sur le bouton crée
+let myHeading = document.querySelector('h1');      //reference sur le titre de la page
 
 
 function setUserName() {
     let myName = prompt('Veuillez saisir votre nom.');
-    localStorage.setItem('nom', myName);
+    localStorage.setItem('nom', myName);                                // demander à l'utilisateur de saisir son nom
     myHeading.textContent = 'Mozilla est cool, ' + myName;
   }
 
@@ -47,16 +47,45 @@ function setUserName() {
   });
 
 
+
+
+
 let myvariable=document.querySelector ('html');
+
 function visitwebsite()
 {  let Mychoice = prompt ('voulez vous visiter le fameux site xxxvidsxxx? o/n'); 
    localStorage.setItem('reponse',Mychoice);
-
+                                                                                        //visiter le site ou non
 
 }
 visitwebsite();
 let choice=localStorage.getItem('reponse');
 
+if (choice ==='o') { window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); } //redirect vers youtube
 
-if (choice ==='o') { window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); } 
+
+
+
+
+
+
+
+
+let Mypolice=document.getElementById('police');
+
+function changepolice ()
+{document.querySelector('p').style.color='blue' ;            //changer la police des titres et paragraphe
+                                                                           
+document.querySelector('h2').style.color='green' ;  
+document.querySelector('p').style.fontFamily ='courier' ; 
+document.querySelector('h2').style.fontFamily ='Comic Sans'; 
+document.getElementsByClassName("troisiemetitre").style.color='green' ;}
+
+Mypolice.addEventListener ('click' , function() { 
+  changepolice ();
+
+});
+
+
+
 
